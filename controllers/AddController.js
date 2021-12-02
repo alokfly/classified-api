c783e1cf55fccec7f6aae66894ad50b9fb5fe2c3
alok2110 <aloksaxena755@gmail.com>
2021-12-02 14:30:43 +0530

    new change

--- controllers/AddController.js
+++ controllers/AddController.js
@@ -118,7 +118,7 @@ module.exports.deleteAd = async (req, res) => {
 
 module.exports.myAds = async (req, res) => {
   try {
-    const ads = await Add.findById({ userId: ObjectId(req.user._id) });
+    const ads = await Add.findOne({ userId: ObjectId(req.user) });
     return res.status(200).json(ads);
   } catch (error) {
     console.log(error);
