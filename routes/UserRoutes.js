05d5c4a2081007c0877d82d1d4d867b72842608d
alok2110 <aloksaxena755@gmail.com>
2021-12-04 10:56:05 +0530

    new change

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -26,6 +26,8 @@ const {
 router.post("/register", registerValiations, register);
 router.post("/login", loginValiations, login);
 router.get("/loggedUserDetail", auth, loggedUserDetail);
+router.post("/email-send", emailSend);
+router.post("/change-password", changePassword);
 router.post("/editUser", upload.single("myField"), auth, editUser);
 router.get("/showAllUser", showAllUser);
 
